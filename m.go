023bf00ee17641ab8b0120f67ab4de552692f90f
Caio Starke <caiostarke@hotmail.com>
2023-04-05 19:28:29 -0300

    improving existing features

--- m.go
+++ m.go
@@ -10,12 +10,15 @@ func main() {
 	p(u)
 }
 
+// kaiou j3j123 njkdol kdapspd (y = kdkjmn, z = kekas), ;adskl kdks di1!
 func x(y,z string) []byte {
 	l := z + y
 
 	return []byte(l)
 }
 
+
+// ksekl.p ldkseri (o = klak), dkllooe jewu
 func p(o []byte) {
 	for i := 0; i < 25; i++ {
 		fmt.Println(o)
